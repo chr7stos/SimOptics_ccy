8375bcdd13a35d6c82fb6409133e02dd33e9cf9e
Christos C. Y <christoscy@protonmail.ch>
2015-11-06 14:43:04 +0100

    measured_spectra changed
    added to path of matlab, now all the experimental spectra are inside
    this folder

--- loaddata.m
+++ loaddata.m
@@ -1,4 +1,4 @@
-function res = loaddata(filename,ncols,commentsign) %(filename)
+ function res = loaddata(filename,ncols,commentsign) %(filename)
 % loads ASCII-file in a data matrix with
 % ncols: number of colums
 % commentsdign: sign for headerinfo
@@ -6,8 +6,11 @@ function res = loaddata(filename,ncols,commentsign) %(filename)
 % 06.07.2007 WT
 % 06.11.2015 CCY
 
+% The measured_spectra folder is added to the path, otherwise it does not
+% work
 
-fin = fopen(filename,'r'); % open file with error handling
+
+fin = fopen(filename,'r'); % open file with error handling 
 if fin < 0
    error(['Could not open ',filename,' for input']); % If fopen cannot open the file, then fileID is -1.
 end
